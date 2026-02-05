4d817e1cc9af44d14a9cdaaae82a0997177ce9ee
lubbacris <lewis.karume@icloud.com>
2026-02-05 23:52:22 +0200

    W04 Code: Linked Lists assigment

--- week04/code/LinkedList.cs
+++ week04/code/LinkedList.cs
@@ -32,7 +32,23 @@ public class LinkedList : IEnumerable<int>
     /// </summary>
     public void InsertTail(int value)
     {
-        // TODO Problem 1
+        // Problem 1: InsertTail
+        // Create new node
+    Node newNode = new(value);
+    
+    // If the list is empty, then point both head and tail to the new node.
+    if (_tail is null)
+    {
+        _head = newNode;
+        _tail = newNode;
+    }
+    // If the list is not empty, then only tail will be affected.
+    else
+    {
+        newNode.Prev = _tail; // Connect new node to previous tail
+        _tail.Next = newNode; // Connect the previous tail to new node
+        _tail = newNode;      // Update the tail to point to new node
+    }
     }
 
 
@@ -53,18 +69,32 @@ public class LinkedList : IEnumerable<int>
         // will be affected.
         else if (_head is not null)
         {
-            _head.Next!.Prev = null; // Disconnect the second node from the first node
-            _head = _head.Next; // Update the head to point to the second node
+            _head.Next!.Prev = null; // Disconnect second node from first node
+            _head = _head.Next; // Update head to point to second node
         }
     }
 
 
     /// <summary>
-    /// Remove the last node (i.e. the tail) of the linked list.
+    /// Remove last node of the linked list.
     /// </summary>
     public void RemoveTail()
     {
-        // TODO Problem 2
+        // Problem 2: RemoveTail
+        // If the list has only one item in it, then set head and tail 
+        // to null resulting in an empty list.
+    if (_head == _tail)
+    {
+        _head = null;
+        _tail = null;
+    }
+    // If list has more than one item in it, then only the tail
+    // will be affected.
+    else if (_tail is not null)
+    {
+        _tail.Prev!.Next = null; // Disconnect second to last node from the last node
+        _tail = _tail.Prev;      // Update tail to point to the second to last node
+    }
     }
 
     /// <summary>
@@ -108,7 +138,32 @@ public class LinkedList : IEnumerable<int>
     /// </summary>
     public void Remove(int value)
     {
-        // TODO Problem 3
+        // Problem 3: Remove
+        Node? curr = _head;
+    while (curr is not null)
+    {
+        if (curr.Data == value)
+        {
+            // Case 1 - The node to remove is the head
+            if (curr == _head)
+            {
+                RemoveHead();
+            }
+            // Case 2 - The node to remove is the tail
+            else if (curr == _tail)
+            {
+                RemoveTail();
+            }
+            // Case 3 - The node is in the middle
+            else
+            {
+                curr.Prev!.Next = curr.Next; // Connect previous node to next node
+                curr.Next!.Prev = curr.Prev; // Connect next node to previous node
+            }
+            return; // Stop searching after removing the first match
+        }
+        curr = curr.Next;
+    }
     }
 
     /// <summary>
@@ -116,7 +171,16 @@ public class LinkedList : IEnumerable<int>
     /// </summary>
     public void Replace(int oldValue, int newValue)
     {
-        // TODO Problem 4
+        // Problem 4: Replace
+        Node? curr = _head;
+    while (curr is not null)
+    {
+        if (curr.Data == oldValue)
+        {
+            curr.Data = newValue;
+        }
+        curr = curr.Next; // Continue searching
+    }
     }
 
     /// <summary>
@@ -146,8 +210,13 @@ public class LinkedList : IEnumerable<int>
     /// </summary>
     public IEnumerable Reverse()
     {
-        // TODO Problem 5
-        yield return 0; // replace this line with the correct yield return statement(s)
+        // Problem 5: Reverse
+        var curr = _tail; // Start at the end
+        while (curr is not null)
+    {
+        yield return curr.Data; // Provide (yield) each item
+        curr = curr.Prev;       // Go backward in the linked list
+    }
     }
 
     public override string ToString()
